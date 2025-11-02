de36ab27d4b8a89413fd712c1ec1f7739c9e6130
iTzManic <itzizmanic@gmail.com>
2025-11-02 17:39:01 -0600

    12

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,15 +1,20 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId } from '../env'
+// 1. IMPORT THE NEW readToken
+import { apiVersion, dataset, projectId, readToken } from '../env'
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
   
-  // This is fine. It means we get fresh data, but it's still public.
-  useCdn: false,
+  useCdn: false, // Must be false for live preview and server fetching
+  
+  // 2. USE THE TOKEN FOR ALL SERVER-SIDE REQUESTS
+  // This will authenticate the Vercel server
+  token: readToken,
 
+  // These settings are required for Live Preview
   perspective: 'published',
   stega: {
     enabled: false,
